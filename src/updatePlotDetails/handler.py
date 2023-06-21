e9306550c8133a9ae8619b7fb2fe19bf84d72d86
sudhirp <sudhir_patavardhan@intuit.com>
2023-06-21 23:32:18 +0530

    addPLot

--- src/updatePlotDetails/handler.py
+++ src/updatePlotDetails/handler.py
@@ -1,6 +1,22 @@
-import json
-def handler(event, context):
-    # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+import boto3
 
-    return {}
+def handler(event, context):
+    # Extract plot details from the event
+    plot_details = {
+        'plotNumber': event['plotNumber'],
+        'ownerName': event['ownerName'],
+        'dimensions': event['dimensions'],
+        'area': event['area'],
+        'ownershipStartDate': event['ownershipStartDate'],
+        'releaseStatus': event['releaseStatus']
+    }
+
+    # Insert plot details into DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('SMVRL-plot-LJC9WGC4FCR')
+    table.put_item(Item=plot_details)
+
+    return {
+        'statusCode': 200,
+        'body': 'Plot details inserted successfully'
+    }
